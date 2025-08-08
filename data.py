1a16b4be2b0f03be675a3b7a938656fb198a67ba
Trulyiconic Tech <trulyiconictech@gmail.com>
2025-08-08 12:18:43 +0545

    finalized for site
    Updated everything

--- data.py
+++ data.py
@@ -2,9 +2,12 @@ import os
 import json
 
 image_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp']
+video_extensions = ['.mp4', '.mov', '.avi', '.mkv', '.webm', '.flv']
+all_media_extensions = image_extensions + video_extensions
+
 output_dir = 'database'
 media_dir = 'media'
-notice_dir = 'notice'
+others_dir = 'others'
 
 os.makedirs(output_dir, exist_ok=True)
 
@@ -20,32 +23,58 @@ for subfolder in os.listdir(media_dir):
             if os.path.isfile(file_path) and os.path.splitext(file)[1].lower() in image_extensions:
                 relative_path = os.path.join(media_dir, subfolder, file).replace('\\', '/')
                 images.append(relative_path)
-        image_files_by_folder[subfolder] = images  # even if empty list
+        image_files_by_folder[subfolder] = images
 
 with open(media_json_path, 'w') as f:
     json.dump({"images": image_files_by_folder}, f, indent=4)
 
+others_json_path = os.path.join(output_dir, 'others.json')
+others_dict = {}
+
+if os.path.isdir(others_dir):
+    for file in os.listdir(others_dir):
+        file_path = os.path.join(others_dir, file)
+        name, ext = os.path.splitext(file)
+        if os.path.isfile(file_path) and ext.lower() in all_media_extensions:
+            key = name
+            value = os.path.join('others', file).replace('\\', '/')
+            others_dict[key] = value
+
+    with open(others_json_path, 'w') as f:
+        json.dump(others_dict, f, indent=4)
+
 notice_json_path = os.path.join(output_dir, 'notice.json')
 notices = []
+notice_dir = 'notice'
 
 for folder_name in os.listdir(notice_dir):
     folder_path = os.path.join(notice_dir, folder_name)
     if os.path.isdir(folder_path) and '-' in folder_name:
         date_part, title_part = folder_name.split('-', 1)
-        title = title_part.replace('_', ' ').strip().capitalize()
-        unique_id = title.replace(' ', '-').lower()
-        files = []
+        formatted_date = f"{date_part[:4]}-{date_part[4:6]}-{date_part[6:]}"
+        raw_title = title_part.replace('_', ' ').strip()
+        title_lower = raw_title.lower()
+        title_title_case = raw_title.title()
+
+        file_entries = []
         for file in os.listdir(folder_path):
             file_path = os.path.join(folder_path, file)
             if os.path.isfile(file_path):
-                files.append(file_path.replace('\\', '/'))
-        notice = {
-            "id": unique_id,
-            "date": date_part,
-            "title": title,
-            "files": files
-        }
-        notices.append(notice)
+                ext = os.path.splitext(file)[1].lstrip('.').lower()
+                relative_path = os.path.join('notice', folder_name, file).replace('\\', '/')
+                file_entries.append({
+                    "file_name": title_title_case,
+                    "file_type": ext,
+                    "file_path": relative_path
+                })
+
+        if file_entries:
+            notice = {
+                "date": formatted_date,
+                "title": title_lower,
+                "files": file_entries
+            }
+            notices.append(notice)
 
 with open(notice_json_path, 'w', encoding='utf-8') as f:
     json.dump({"notices": notices}, f, indent=4)
@@ -61,8 +90,3 @@ else:
 
 with open(version_file, 'w') as vf:
     json.dump({"version": version}, vf, indent=4)
-
-os.system("git add .")
-os.system(f'git commit -m "{version}"')
-os.system("git push")
-
